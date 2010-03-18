48ef0143b5f5936f3869ef4c19eaf3468b2414bc
Andreas Blixt <andreas@blixt.org>
2010-03-18 22:48:45 +0100

    Removed commented code.

--- app/app.py
+++ app/app.py
@@ -33,56 +33,6 @@ class HomeHandler(webapp.RequestHandler):
             blixt.appengine.cache.set('app', html, 3600)
         self.response.out.write(html)
 
-##class ImageHandler(webapp.RequestHandler):
-##    def get(self, image_id):
-##        # Build a key that identifies the requested image and size.
-##        cache_key = 'image_%s' % (image_id)
-##
-##        # Attempt to get the image from the memory cache.
-##        data = memcache.get(cache_key)
-##        if data:
-##            self.response.headers['Content-Type'] = str(data['content_type'])
-##            self.response.out.write(data['data'])
-##            return
-##
-##        # Get the image from the data store.
-##        try:
-##            image = multifarce.model.Image.get_by_id(int(image_id))
-##            if not image:
-##                raise multifarce.NotFoundError('The specified image does not '
-##                                               'exist.', 'IMAGE_NOT_FOUND')
-##            image_data = image.get_image_data(size)
-##
-##            # Cache the image.
-##            memcache.set(cache_key, {'data': image_data.data,
-##                                     'content_type': image_data.content_type})
-##
-##            self.response.headers['Content-Type'] = str(image_data.content_type)
-##            self.response.out.write(image_data.data)
-##        except:
-##            self.error(404)
-##
-##class UploadHandler(webapp.RequestHandler):
-##    def post(self):
-##        user = multifarce.model.User.get_current(self)
-##        if not user:
-##            raise multifarce.NotLoggedInError('You must be logged in to upload '
-##                                              'images.', 'MUST_BE_LOGGED_IN')
-##
-##        frame_id = int(self.request.get('frame_id'))
-##        frame = multifarce.model.Frame.get(frame_id)
-##        if not frame:
-##            raise multifarce.NotFoundError('Frame does not exist.',
-##                                           'FRAME_NOT_FOUND')
-##        if frame.user_id != user.key.id():
-##            raise multifarce.InvalidOperation('Cannot upload image to others\' '
-##                                              'frames.', 'ACCESS_VIOLATION')
-##
-##        img_data = multifarce.model.Image.upload(self.request.get('image'))
-##
-##        frame.image_id = img_data.key().id()
-##        frame.put()
-
 class DummyHandler(webapp.RequestHandler):
     def get(self):
         pass
@@ -93,8 +43,6 @@ def main():
         ('/api/(\\w*)', multifarce.view.MultifarceService),
         # Specific to *.multifarce.com
         ('/googlee3ef4462fc530463.html', DummyHandler),
-##        ('/image/(\\d+)', ImageHandler),
-##        ('/upload', UploadHandler),
     ])
     wsgiref.handlers.CGIHandler().run(application)
 
